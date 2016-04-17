3f0b17fb633a8f21a7bce6a1872a69b21f3e06b4
Brenton Leasure <brenton.leasure@hotmail.com>
2016-04-17 14:12:30 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var server = express();
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static(__dirname + "/public"));
+
+server.get("/", function(req, res){
+	res.sendFile("/main.html", {root: "./public/home"})
+});
+
+
+var port = 3000;
+server.listen(port, function(){
+  console.log('Server running on port ' + port);
+})	
