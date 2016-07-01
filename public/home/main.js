bc69566d1d7de2b7272f89ee7b1d049e60e76fcc
Brenton Leasure <brenton.leasure@hotmail.com>
2016-07-01 17:06:14 -0600

    scss and javascript changes

--- public/home/main.js
+++ public/home/main.js
@@ -1,14 +1,22 @@
 var previousPageID = "";
 var previousButtonID = "";
+var colors = ['#1072b8', '#e44d26', '#90c53f'];
 
-function NavbarClick(id){
+function NavbarClick(id, idNum){
 	if(previousButtonID == ""){
 		document.getElementById(id).classList.add("active-button");
-		document.getElementById("about-me").classList.remove("active-button");
+		document.getElementById("about-me").classList.remove("active-button"); 
+
+		document.getElementById(id).style.backgroundColor = colors[idNum];
+		document.getElementById("about-me").style.backgroundColor = "black";
 	}else{
 		document.getElementById(id).classList.add("active-button");
 		document.getElementById(previousButtonID).classList.remove("active-button");
+
+		document.getElementById(id).style.backgroundColor = colors[idNum];
+		document.getElementById(previousButtonID).style.backgroundColor = "black";
 	}
+
 	previousButtonID = id;
 }
 
