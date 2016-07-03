cfa3724634b1b5cad897ce521b6ba4d11e709e55
Brenton Leasure <brenton.leasure@hotmail.com>
2016-07-02 22:52:57 -0600

    finished changes. Probably needs work, but I'm tired

--- public/home/main.js
+++ public/home/main.js
@@ -2,26 +2,30 @@ var previousPageID = "pageOne";
 var previousButtonID = "about-me";
 var colors = ['#1072b8', '#e44d26', '#90c53f'];
 
-function NavbarClick(id, idNum){
-	if(previousButtonID == ""){
-		document.getElementById("about-me").classList.remove("active-button");
-		document.getElementById(id).classList.add("active-button"); 
+// function NavbarClick(id, idNum){
+// 	if(previousButtonID == ""){
+// 		document.getElementById("about-me").classList.remove("active-button");
+// 		document.getElementById(id).classList.add("active-button"); 
 
-		document.getElementById(id).style.backgroundColor = colors[idNum];
-		document.getElementById("about-me").style.backgroundColor = "black";
-	}else{
-		document.getElementById(previousButtonID).classList.remove("active-button");
-		document.getElementById(id).classList.add("active-button");
+// 		document.getElementById(id).style.backgroundColor = colors[idNum];
+// 		document.getElementById("about-me").style.backgroundColor = "black";
+// 	}else{
+// 		document.getElementById(previousButtonID).classList.remove("active-button");
+// 		document.getElementById(id).classList.add("active-button");
 
-		document.getElementById(id).style.backgroundColor = colors[idNum];
-		document.getElementById(previousButtonID).style.backgroundColor = "black";
-	}
+// 		document.getElementById(id).style.backgroundColor = colors[idNum];
+// 		document.getElementById(previousButtonID).style.backgroundColor = "black";
+// 	}
 
-	previousButtonID = id;
-}
+// 	previousButtonID = id;
+// }
 
 function ActivatePage(id){
-		console.log(id);
+		if(id != 'pageOne'){
+			document.getElementById('home-button').style.visibility = 'visible';
+		}else{
+			document.getElementById('home-button').style.visibility = 'hidden';
+		}
 		document.getElementById(previousPageID).classList.remove("active");
 		document.getElementById(id).classList.add("active");
 	previousPageID = id;
