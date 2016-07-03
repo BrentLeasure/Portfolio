b9e75de1664c7f17ce01e5e1d03d40071be4d4c9
Brenton Leasure <brenton.leasure@hotmail.com>
2016-07-03 17:23:55 -0600

    getting close...

--- public/home/main.js
+++ public/home/main.js
@@ -2,23 +2,23 @@ var previousPageID = "pageOne";
 var previousButtonID = "about-me";
 var colors = ['#1072b8', '#e44d26', '#90c53f'];
 
-// function NavbarClick(id, idNum){
-// 	if(previousButtonID == ""){
-// 		document.getElementById("about-me").classList.remove("active-button");
-// 		document.getElementById(id).classList.add("active-button"); 
+function NavbarClick(id, idNum){
+	if(previousButtonID == ""){
+		document.getElementById("about-me").classList.remove("active-button");
+		document.getElementById(id).classList.add("active-button"); 
 
-// 		document.getElementById(id).style.backgroundColor = colors[idNum];
-// 		document.getElementById("about-me").style.backgroundColor = "black";
-// 	}else{
-// 		document.getElementById(previousButtonID).classList.remove("active-button");
-// 		document.getElementById(id).classList.add("active-button");
+		document.getElementById(id).style.backgroundColor = colors[idNum];
+		document.getElementById("about-me").style.backgroundColor = "black";
+	}else{
+		document.getElementById(previousButtonID).classList.remove("active-button");
+		document.getElementById(id).classList.add("active-button");
 
-// 		document.getElementById(id).style.backgroundColor = colors[idNum];
-// 		document.getElementById(previousButtonID).style.backgroundColor = "black";
-// 	}
+		document.getElementById(id).style.backgroundColor = colors[idNum];
+		document.getElementById(previousButtonID).style.backgroundColor = "black";
+	}
 
-// 	previousButtonID = id;
-// }
+	previousButtonID = id;
+}
 
 function ActivatePage(id){
 		if(id != 'pageOne'){
