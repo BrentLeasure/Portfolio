def5a1d3c57340f8104538e6f7e60a4e03a78835
Brenton Leasure <brenton.leasure@hotmail.com>
2016-07-02 22:18:53 -0600

    lots of changes. Many more to come.

--- public/home/main.js
+++ public/home/main.js
@@ -1,17 +1,17 @@
-var previousPageID = "";
-var previousButtonID = "";
+var previousPageID = "pageOne";
+var previousButtonID = "about-me";
 var colors = ['#1072b8', '#e44d26', '#90c53f'];
 
 function NavbarClick(id, idNum){
 	if(previousButtonID == ""){
-		document.getElementById(id).classList.add("active-button");
-		document.getElementById("about-me").classList.remove("active-button"); 
+		document.getElementById("about-me").classList.remove("active-button");
+		document.getElementById(id).classList.add("active-button"); 
 
 		document.getElementById(id).style.backgroundColor = colors[idNum];
 		document.getElementById("about-me").style.backgroundColor = "black";
 	}else{
-		document.getElementById(id).classList.add("active-button");
 		document.getElementById(previousButtonID).classList.remove("active-button");
+		document.getElementById(id).classList.add("active-button");
 
 		document.getElementById(id).style.backgroundColor = colors[idNum];
 		document.getElementById(previousButtonID).style.backgroundColor = "black";
@@ -21,12 +21,8 @@ function NavbarClick(id, idNum){
 }
 
 function ActivatePage(id){
-	if(previousPageID == ""){
-		document.getElementById(id).classList.add("active");
-		document.getElementById("pageOne").classList.remove("active");
-	}else{
-		document.getElementById(id).classList.add("active");
+		console.log(id);
 		document.getElementById(previousPageID).classList.remove("active");
-	}
+		document.getElementById(id).classList.add("active");
 	previousPageID = id;
 }
