676bb34e08dce13263c330c35f64445e47bd3ff3
Brenton Leasure <brenton.leasure@hotmail.com>
2016-07-27 22:46:06 -0600

    lots of issues with nav bar shape changes. Will figure out how to solve them...

--- public/home/main.js
+++ public/home/main.js
@@ -1,43 +1,69 @@
 var previousPageID = "pageOne", previousButtonID = "home", colors = ['#1072b8', '#e44d26', '#90c53f'], navButtons = document.getElementsByClassName('nav-buttons');
 
-function changeButtonLocation(isWindoResize){
-	    if(window.innerWidth > 751){
+function changeButtonLocation(bool){
+	if(window.innerWidth > 751){
     	//if the window is BIGGER than 751px, this will fire
-    	if(previousButtonID == 'home' && isWindoResize){
+    	if(bool){
     		//If the home button was clicked last, then it will adjust the 
     		//location of the buttons as if they were on the main page
 	    	document.getElementById('about-me').style.top = '70px';
 			document.getElementById('skills').style.top = '0px';
 			document.getElementById('projects').style.top = '70px';
+
+			//loop through and set nav buttons to circles
+			for(var button = 0; button < navButtons.length; button++){
+				console.log("testing");
+				navButtons[button].style.height = '125px';
+				navButtons[button].style.borderRadius = '100%';
+			}
+
 		}else{
 			//If the home button was NOT clicked last, then it will adjust the 
     		//location of the buttons as if they were NOT on the main page
 			document.getElementById('about-me').style.top = '10px';
 			document.getElementById('skills').style.top = '10px';
 			document.getElementById('projects').style.top = '10px';
+
+			//loop through and set nav buttons to rectangles
+			for(var button = 0; button < navButtons.length; button++){
+				console.log("testing");
+				navButtons[button].style.height = '50px';
+				navButtons[button].style.borderRadius = '25px';
+			}
 		}
     }else{
     	//if the window is SMALLER than 751px, this will fire
-    	if(previousButtonID == 'home'){
+    	if(bool){
     		//If the home button was clicked last, then it will adjust the 
     		//location of the buttons as if they were on the main page
     		document.getElementById('about-me').style.top = '80px';
 			document.getElementById('skills').style.top = '-70px';
 			document.getElementById('projects').style.top = '-120px';
+
+			//loop through and set nav buttons to circles
+			for(var button = 0; button < navButtons.length; button++){
+				console.log("testing");
+				navButtons[button].style.height = '125px';
+				navButtons[button].style.borderRadius = '100%';
+			}
+
     	}else{
     		//If the home button was NOT clicked last, then it will adjust the 
     		//location of the buttons as if they were NOT on the main page
     		document.getElementById('about-me').style.top = '10px';
 			document.getElementById('skills').style.top = '-40px';
 			document.getElementById('projects').style.top = '-89px';
+			
+			//loop through and set nav buttons to rectangles
+			for(var button = 0; button < navButtons.length; button++){
+				console.log("testing");
+				navButtons[button].style.height = '50px';
+				navButtons[button].style.borderRadius = '25px';
+			}
     	}
     }
 }
 
-window.onresize = function(event) {
-	//if window resizes, this will run
-	changeButtonLocation(true);
-};
 
 function resetButtonLocation(){
 	//if the home button is clicked, then this will fire 
@@ -58,12 +84,19 @@ function resetButtonLocation(){
 	previousButtonID = 'home';
 }
 
+
+window.onresize = function(event) {
+	//if window resizes, this will run
+	changeButtonLocation(false);
+};
+
+
+
 function NavbarClick(id, idNum){
 	if(previousButtonID == "home"){
 		//if previous button is 'home', then there is no previous button to reset
 		//this will fire and only set a button's background color
 		document.getElementById(id).classList.add("active-button"); 
-
 		document.getElementById(id).style.backgroundColor = colors[idNum];
 	}else{
 		//If previous button is NOT home, it changes background color to
@@ -77,7 +110,11 @@ function NavbarClick(id, idNum){
 	}
 
 	//changes the buttons location
-	changeButtonLocation(false);
+	if(previousButtonID == 'home'){
+		changeButtonLocation(true);
+	}else{
+		changeButtonLocation(false);
+	}
 	//previous button is set to the button that was clicked
 	previousButtonID = id;
 }
