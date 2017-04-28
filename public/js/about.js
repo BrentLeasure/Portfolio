d916be545966074475d2beae76447be64a009878
Brenton Leasure <brenton.leasure@hotmail.com>
2017-04-27 20:57:32 -0600

    minor css changes in alignment

--- public/js/about.js
+++ public/js/about.js
@@ -1,23 +1,31 @@
-let historyElements = document.getElementById('history-events-list') ,
-historyListItems = historyElements.getElementsByTagName('li');
+let body = document.body, historyElements = document.getElementById('history-events-list') ,
+historyListItems = historyElements.getElementsByTagName('li'), endOfHistory = document.getElementById('end-of-history');
 
 window.addEventListener( 'scroll', function(e) { 
 	if( historyListItems[0].offsetTop != 0) {
 		for( let i = 0;  i < historyListItems.length; i++ ) {
-			if ( window.pageYOffset >= historyListItems[i].offsetTop ) {
+			if ( window.pageYOffset >= historyListItems[i].offsetTop - 55 ) {
 				if( i % 2 == 0 ) {
 					historyListItems[i].style.transform = "translateX( 0% )";
 				} else {
-					historyListItems[i].style.transform = "translateX( 135% )";
+					historyListItems[i].style.transform = "translateX( 130% )";
 				}
 			} else if (historyListItems[i].style.transform != "") {
 				if( i % 2 == 0 ) {
-					historyListItems[i].style.transform = "translateX( -150% )";
+					historyListItems[i].style.transform = "translateX( -160% )";
 				} else {
-					historyListItems[i].style.transform = "translateX( 235% )";
+					historyListItems[i].style.transform = "translateX( 250% )";
 				}
 			}
 		}
+		console.log( body.scrollHeight);
+		console.log( body.scrollTop + window.innerHeight  + 100 );
+		if ( body.scrollHeight <= ( body.scrollTop + window.innerHeight  ) ) {
+			endOfHistory.style.opacity = 1;
+		} else if ( endOfHistory.style.opacity == 1 ) {
+			endOfHistory.style.opacity = 0;
+		}
 	}
+
 
 });
