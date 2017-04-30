982a4587e32a74f5e1249b315e5eb88f071e7cde
Brenton Leasure <brenton.leasure@hotmail.com>
2017-04-30 14:47:30 -0600

    reorganizing page layout

--- public/js/about.js
+++ public/js/about.js
@@ -2,6 +2,13 @@ let body = document.body, historyElements = document.getElementById('history-eve
 historyListItems = historyElements.getElementsByTagName('li'), endOfHistory = document.getElementById('end-of-history');
 
 window.addEventListener( 'scroll', function(e) { 
+	EnterHistoryElements();
+
+
+});
+
+
+function EnterHistoryElements() {
 	if( historyListItems[0].offsetTop != 0) {
 		for( let i = 0;  i < historyListItems.length; i++ ) {
 			if ( window.pageYOffset >= historyListItems[i].offsetTop - 55 ) {
@@ -18,14 +25,11 @@ window.addEventListener( 'scroll', function(e) {
 				}
 			}
 		}
-		console.log( body.scrollHeight);
-		console.log( body.scrollTop + window.innerHeight  + 100 );
+
 		if ( body.scrollHeight <= ( body.scrollTop + window.innerHeight  ) ) {
 			endOfHistory.style.opacity = 1;
 		} else if ( endOfHistory.style.opacity == 1 ) {
 			endOfHistory.style.opacity = 0;
 		}
 	}
-
-
-});
+}
